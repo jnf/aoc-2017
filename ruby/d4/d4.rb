b710bd415cb5dd725d0752c30a4b4f7e57c5c628
jnf <git@has-many.com>
2017-12-04 07:15:21 -0800

    part 2 of day 4 in Ruby

--- ruby/d4/d4.rb
+++ ruby/d4/d4.rb
@@ -1,5 +1,10 @@
-def valid?(line)
+def p1_valid?(line)
   splits = line.split(/\s+/)
+  splits.uniq.length == splits.length
+end
+
+def p2_valid?(line)
+  splits = line.split(/\s+/).map { |split| split.chars.sort.join }
   splits.uniq.length == splits.length
 end
 
@@ -8,7 +13,18 @@ end
   "aa bb cc dd aa",
   "aa bb cc dd aaa"
 ].each do |line|
-  puts "#{ line } is #{ valid?(line) ? 'valid' : 'invalid' }."
+  puts "P1: #{ line } is #{ p1_valid?(line) ? 'valid' : 'invalid' }."
 end
 
-puts File.read("input").each_line.select { |l| valid?(l) }.length
+[
+  "abcde fghij",
+  "abcde xyz ecdab",
+  "a ab abc abd abf abj",
+  "iiii oiii ooii oooi oooo",
+  "oiii ioii iioi iiio"
+].each do |line|
+  puts "P2: #{ line } is #{ p2_valid?(line) ? 'valid' : 'invalid' }."
+end
+
+puts File.read("input").each_line.select { |l| p1_valid?(l) }.length
+puts File.read("input").each_line.select { |l| p2_valid?(l) }.length
