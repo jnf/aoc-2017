3bd1ee10eb5eee893238a5333f947def39ad0920
jnf <jeremy@mty.ai>
2017-12-06 09:12:56 -0800

    bit of cleanup for d3 ruby

--- d3/ruby/d3.rb
+++ d3/ruby/d3.rb
@@ -1,26 +1,22 @@
-INPUT = 312051
-
-def build_pivots(n)
-  pivots = [[2], [4], [6], [8]]
-  o = 1
-  while pivots.last.last < n
-    pivots.map! do |m|
-      m << m.last + (8 * o + m.first - 1)
+def build_pivots(input, seed = [[2], [4], [6], [8]])
+  offset = 1
+  while seed.last.last < input
+    seed.map! do |list|
+      list << list.last + (8 * offset + list.first - 1)
     end
-    o += 1
+    offset += 1
   end
 
-  pivots
+  [offset, seed.map(&:last)]
 end
 
-def p1(n)
-  pivots = build_pivots n
-  grid_size = pivots.first.length
-  pivot_points = pivots.map { |p| p[grid_size - 1]}
-  pivot_distance = pivot_points.map { |p| (p - n).abs }.min
-  "#{n}: distance to pivot: #{pivot_distance}, distance from center: #{grid_size}, total: #{pivot_distance + grid_size}"
+def p1(input = 0)
+  grid_size, pivot_points = build_pivots input
+  pivot_distance = pivot_points.map { |pp| (pp - input).abs }.min
+  "#{input}: distance to pivot: #{pivot_distance}, distance from center: #{grid_size}, total: #{pivot_distance + grid_size}"
 end
 
-[12,23,1024,INPUT].each do |n|
-  puts p1 n
+INPUT = 312051
+[12,23,1024,INPUT].each do |input|
+  puts p1 input
 end
