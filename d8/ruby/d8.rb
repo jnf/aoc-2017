01f324461ee8e0549b4c6fe43a10c2faea0582fe
jnf <git@has-many.com>
2017-12-10 15:07:25 -0800

    day 8, part 2 in Ruby

--- d8/ruby/d8.rb
+++ d8/ruby/d8.rb
@@ -1,4 +1,28 @@
 require 'forwardable'
+class Processor
+  attr_reader :registers, :max_value
+  def initialize
+    @registers = Hash.new { |hash, key| hash[key] = Register.new(key) }
+    @max_value = 0
+  end
+
+  def process!(path)
+    File.read(path).each_line.reduce(registers) do |regs, line|
+      id, method, delta, _if, target_id, test_method, check = line.split /\s+/
+
+      if regs[target_id].send(test_method.to_sym, check.to_i)
+        regs[id].send(method.to_sym, delta.to_i)
+        @max_value = regs[id].value if regs[id].value > max_value
+      end
+
+      regs
+    end
+  end
+
+  def from_file(path)
+    File.read(path).each_line
+  end
+end
 
 class Register
   extend Forwardable
@@ -15,16 +39,7 @@ class Register
 
 end
 
-registers = Hash.new { |hash, key| hash[key] = Register.new(key) }
-File.read("input").each_line.reduce(registers) do |regs, line|
-  id, method, delta, _if, target_id, test_method, check = line.split /\s+/
-
-  target = regs[target_id]
-  if target.send(test_method.to_sym, check.to_i)
-    regs[id].send(method.to_sym, delta.to_i)
-  end
-
-  regs
-end
-
-p registers.max_by { |name, reg| reg.value }[1]
+processor = Processor.new
+processor.process! "input"
+p processor.registers.max_by { |name, reg| reg.value }[1]
+p processor.max_value
